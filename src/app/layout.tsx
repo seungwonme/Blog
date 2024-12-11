a93b19504d86fff8e681dd7aed6d5fe75fa7c8c9
seungwonme <seungwonan.me@gmail.com>
2024-12-11 16:01:26 +0900

    feat: D2Coding으로 폰트 변경
    - https://nextjs.org/docs/app/getting-started/installation 문서 참고
    - 타입스크립트 버전 설정 https://nextjs.org/docs/app/getting-started/installation#set-up-typescript
    - 폰트 설정 https://nextjs.org/docs/app/getting-started/images-and-fonts#local-fonts
    - localFont 불러올 때 public에 넣고 절대 경로로 접근했지만 notfound 에러때문에 app/fonts 파일에 넣어서 상대 경로로 불러옴
    - 공식문서에서는 html의 className에 D2Coding.className을 넣어줬지만 동작하지 않아서 body에 넣음

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,20 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const D2Coding = localFont({
+  src: [
+    {
+      path: "./fonts/D2Coding.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/D2Coding.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
 });
 
 export const metadata: Metadata = {
@@ -23,12 +28,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="ko">
+      <body className={D2Coding.className}>{children}</body>
     </html>
   );
 }
